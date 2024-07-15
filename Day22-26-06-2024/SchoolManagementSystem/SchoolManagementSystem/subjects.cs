abfeea86f06c9c2c559ec1621a10875d5ea54bd3
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-07-15 11:49:22 +0530

    15-07-2024

--- Day22-26-06-2024/SchoolManagementSystem/SchoolManagementSystem/subjects.cs
+++ Day22-26-06-2024/SchoolManagementSystem/SchoolManagementSystem/subjects.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,49 @@ namespace SchoolManagementSystem
         {
             InitializeComponent();
         }
+
+
+        private void FetchData()
+        {
+            string connectionString = null;
+            string sql = null;
+
+            connectionString = "Server=MITHONK\\SQLEXPRESS; Database=SchoolManagementSystem; Trusted_Connection=True; TrustServerCertificate=True;";
+
+            SqlConnection cnn;
+            cnn = new SqlConnection(connectionString);
+            SqlCommand command;
+
+            sql = "SELECT * FROM grades";
+
+            try
+            {
+                cnn.Open();
+                command = new SqlCommand(sql, cnn);
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(sqlDataReader);
+
+                dataGridView.DataSource = dt;
+                sqlDataReader.Close();
+                command.Dispose();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection");
+            }
+        }
+
+        private void ClearData()
+        {
+            gradeName.Clear();
+            gradeGroup.Clear();
+            gradeOrder.Clear();
+            createUpdateBtn.Image = SchoolManagementSystem.Properties.Resources.save;
+            createUpdateBtn.Text = "Create";
+        }
+
     }
 }
